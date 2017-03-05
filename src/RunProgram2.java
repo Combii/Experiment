15523356e46f5dffc4373a949587a71fd9eb1662
Combii <davidstovle@gmail.com>
2017-03-05 13:15:06 +0100

    Added working applet class

---
+++ src/RunProgram2.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by David Stovlbaek
+ * 05 March 2017.
+ */
+public class RunProgram2 extends JApplet {
+
+    public void paint(Graphics g){
+        super.paint(g);
+        g.drawString("This works!", 25, 25);
+    }
+}
